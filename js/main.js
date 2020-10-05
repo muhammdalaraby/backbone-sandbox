6e1a5670c2a2b2cc74114dfb9ea4752834a850e3
muhammadalaraby <muhammad.alaraby@nilecrm.com>
2020-10-05 22:00:09 +0200

    working with collections

--- js/main.js
+++ js/main.js
@@ -3,16 +3,36 @@
 // Later, you'll see how to organize your code into separate
 // files and modules.
 
-    /*
-    |--------------------------------------------------------------------------
-    | Models
-    |--------------------------------------------------------------------------
-    */
+/*
+|--------------------------------------------------------------------------
+| Collections :- a Set of models
+|--------------------------------------------------------------------------
+*/
+ let Song = Backbone.Model.extend();
+ let Songs = Backbone.Collection.extend({
+     model: Song
+ });
+ var songs =  new Songs();
+ 
+ // Add new song at index 0
+ songs.add(new Song({title: "song1",genre:"Jazz",downloads:110}),{at:0});
 
-// communicate with server
-/**
- * fetch() -- GET :- Get the model from the server
- * save() -- POST :- Insert the model to the server
- * save() -- PUT :- update the model to the server
- * destroy() -- DELETE :- Destroy the model on the server
- */
+ // push model to end of array
+ songs.push(new Song({title:"song2",genre:"Jazz",downloads:90}));
+
+ // Search on the collections
+
+ let jaxxSongs = songs.where({genre:"Jazz"});  // return array of objects
+ let firstSongs = songs.findWhere({genre:"Jazz"});
+
+ // Filter results upon downloads number
+ let topDownloads = songs.filter(function(song){
+     return song.get("downloads") > 100;
+ });
+
+ console.log("TopDownload: ", topDownloads);
+
+ // iterate through the current collections
+ songs.each(function(song){
+    console.log(song);
+ })
