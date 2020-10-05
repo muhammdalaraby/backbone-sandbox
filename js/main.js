817ff9d6fd6d10244e4f1dd26bda61e9990eff00
muhammadalaraby <muhammad.alaraby@nilecrm.com>
2020-10-05 17:59:09 +0200

    Models

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// In the first few sections, we do all the coding here.
+// Later, you'll see how to organize your code into separate
+// files and modules.
+
+    /*
+    |--------------------------------------------------------------------------
+    | Models
+    |--------------------------------------------------------------------------
+    */
