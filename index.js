e0b0a56a0c7c3f710005d7ae0891e511439e2521
muhammadalaraby <muhammad.alaraby@nilecrm.com>
2020-10-05 16:53:59 +0200

    Views

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-let Person = Backbone.View.extend(); // new backbone model 
-let person  = new Person(); // new model instance
+$(document).ready(function(){
+
+    // View
+    
+    // Create new backbone view
+    let Person = Backbone.View.extend({
+        // Change tag name
+        tagName : 'span',
+        className: "new class",
+        id:"new class id",
+
+        initialize: function(){
+            this.render(); // render view
+        },
+
+
+        // Render function
+        render: function(){
+            // this,$el :- is a jquery object refer to dom 
+            this.$el.html("here is custom message");
+            // el:- refer to dom element
+            // $el :- refer to jquery object
+            console.log(this.el);
+            console.log(this.$el);
+        },
+    }); 
+    
+    let person = new Person();
+});
+
