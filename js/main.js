1e427f38f166299f08344f093dc22e273e5a1bed
muhammadalaraby <muhammad.alaraby@nilecrm.com>
2020-10-05 22:04:32 +0200

    Connect to the server using collections

--- js/main.js
+++ js/main.js
@@ -8,31 +8,15 @@
 | Collections :- a Set of models
 |--------------------------------------------------------------------------
 */
- let Song = Backbone.Model.extend();
- let Songs = Backbone.Collection.extend({
-     model: Song
- });
- var songs =  new Songs();
- 
- // Add new song at index 0
- songs.add(new Song({title: "song1",genre:"Jazz",downloads:110}),{at:0});
+// Connecting to the server
 
- // push model to end of array
- songs.push(new Song({title:"song2",genre:"Jazz",downloads:90}));
+let Song = Backbone.Model.extend();
 
- // Search on the collections
+let Songs = Backbone.Collection.extend({
+    model:Song,
+    url:"api/spongs",
+});
 
- let jaxxSongs = songs.where({genre:"Jazz"});  // return array of objects
- let firstSongs = songs.findWhere({genre:"Jazz"});
-
- // Filter results upon downloads number
- let topDownloads = songs.filter(function(song){
-     return song.get("downloads") > 100;
- });
-
- console.log("TopDownload: ", topDownloads);
-
- // iterate through the current collections
- songs.each(function(song){
-    console.log(song);
- })
+// Fetch data for the server
+let songs =  new Songs();
+songs.fetch();
