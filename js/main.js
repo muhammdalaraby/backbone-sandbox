a8c9543d93adf1a6abdd1ff48fac3df1ac71288b
muhammadalaraby <muhammad.alaraby@nilecrm.com>
2020-10-06 15:57:36 +0200

    Templating

--- js/main.js
+++ js/main.js
@@ -16,77 +16,26 @@
 // Create  new song model
 let Song = Backbone.Model.extend();
 
-// Create new songs collection
-let Songs = Backbone.Collection.extend({
-    model: Song
-});
-
-// Create new song view
 let SongView = Backbone.View.extend({
-    tagName:"li", 
-
     render:function(){
-        this.$el.html(this.model.get('title'));
+        // Define template
+          let template = _.template($("#songTemplate").html());  
+        //associate template with html 
+        let html = template(this.model.toJSON());
 
-        // add id for each song
-        this.$el.attr("id",this.model.id);
+        // Display template on the dom
+        this.$el.html(html);
+
         return this;
     }
-});
+})
 
-// Create new songs view
-let SongsView = Backbone.View.extend({
-    tagName:"ul",
-    
-      // Update listeners number upon any change in model
-      initialize:function(){
-          // liseten to add event
-        this.model.on("add",this.onSongAdded,this);
-
-        // listen to remove event
-        this.model.on('remove',this.onSongRemove,this);
-    },
-
-    onSongAdded : function (song){
-        let songView =  new SongView({model: song});
-
-        // append to songs list
-        this.$el.append(songView.render().$el);
-    },
-
-    onSongRemove:function(song){
-       //find the dom element with id && remove
-    //    this.$el.find("li#"+sing.id).remove();
-    this.$("#li"+song.id).remove();
-    },
-
-    render:function (){
-        // Store this in slef var to prevent throw an error
-        const SELF = this;
-
-        // Iterate through collection models
-        this.model.each(function(song){
-            let songView =  new SongView({model:song});
-            // append song view element to the last item
-            SELF.$el.append(songView.render().$el);
-        });
-    }
-});
-
-
-
-// Fill songs data
-let songs = new Songs([
-    new Song({id:1,title:"Blue in Green"}),
-    new Song({id:2,title:"Blue in Green"}),
-    new Song({id:4,title:"Blue in Green"}),
-]);
-
+let song = new Song({title: "Blue in Green",plays:1001});
 
 // create song view insance
-let songsView = new SongsView({el:"#container",model:songs});
+let songView = new SongView({el:"#container",model:song});
 
 
 // render song view
-songsView.render();
+songView.render();
 
